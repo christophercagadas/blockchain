5da25d8686ad899b4acd3a3b07f3088691563c32
christophercagadas <christophercagadas@gmail.com>
2020-04-17 20:28:06 +0800

    cryptochain
    PASS ./crypto-hash.test.js
    FAIL ./block.test.js
      ● Test suite failed to run
    
        C:/Users/LordChris/BLOCKCHAIN/cryptochain/config.js: Unexpected token (8:10)
    
          Jest encountered an unexpected token
          This usually means that you are trying to import a file which Jest cannot parse, e.g. it's not plain JavaScript.
          By default, if Jest sees a Babel config, it will use that to transform your files, ignoring "node_modules".
          Here's what you can do:
           • To have some of your "node_modules" files transformed, you can specify a custom "transformIgnorePatterns" in your config.
           • If you need a custom transformation specify a "transform" option in your config.
           • If you simply want to mock your non-JS modules (e.g. binary assets) you can stub them out with the "moduleNameMapper" config option.
          You'll find more details and examples of these config options in the docs:
          https://jestjs.io/docs/en/configuration.html
          Details:
             6 |     hash: 'hash-one',
             7 |     difficulty: INITIAL_DIFFICULTY,
          >  8 |     nonce = 0,
               |           ^
             9 |     data: []
            10 | };
            11 |
    
    FAIL ./blockchain.test.js
      ● Test suite failed to run
    
        C:/Users/LordChris/BLOCKCHAIN/cryptochain/config.js: Unexpected token (8:10)
    
          Jest encountered an unexpected token
          This usually means that you are trying to import a file which Jest cannot parse, e.g. it's not plain JavaScript.
          By default, if Jest sees a Babel config, it will use that to transform your files, ignoring "node_modules".
          Here's what you can do:
           • To have some of your "node_modules" files transformed, you can specify a custom "transformIgnorePatterns" in your config.
           • If you need a custom transformation specify a "transform" option in your config.
           • If you simply want to mock your non-JS modules (e.g. binary assets) you can stub them out with the "moduleNameMapper" config option.
          You'll find more details and examples of these config options in the docs:
          https://jestjs.io/docs/en/configuration.html
          Details:
             6 |     hash: 'hash-one',
             7 |     difficulty: INITIAL_DIFFICULTY,
          >  8 |     nonce = 0,
               |           ^
             9 |     data: []
            10 | };
            11 |
    
    Test Suites: 2 failed, 1 passed, 3 total
    Tests:       2 passed, 2 total
    Snapshots:   0 total
    Time:        6.521s
    Ran all test suites.

---
+++ block.js
@@ -0,0 +1,35 @@
+const { GENESIS_DATA } = require('./config');
+const cryptoHash = require('./crypto-hash');
+
+class Block {
+    constructor({timestamp, lastHash, hash, data, nonce, difficulty}) {
+        this.timestamp = timestamp;
+        this.lastHash = lastHash;
+        this.hash = hash;
+        this.data = data;
+        this.nonce = nonce;
+        this.difficulty = difficulty;
+    }
+
+    static genesis() {
+        return new this(GENESIS_DATA);
+    }
+
+    static mineBlock({ lastBlock, data }){
+        let hash, timestamp;
+        const lastHash = lastBlock.hash;
+        const { difficulty } = lastBlock;
+        let nonce = 0;
+
+        do {
+            nonce++;
+            timestamp = Date.now();
+            hash = cryptoHash(timestamp, lastHash, data, nonce, difficulty);
+
+        } while (hash.substring(0,difficulty) !== '0'.repeat(difficulty));
+
+        return new this({ timestamp, lastHash, data, difficulty, nonce, hash });
+    }
+}
+
+module.exports = Block;
